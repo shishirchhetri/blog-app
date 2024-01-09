4e94eafe2988a978fd67dc82b2ddff8b325d71f5
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-09 21:14:03 +0545

    created optional rendering for admin and login page

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,31 +1,63 @@
-import Link from 'next/link';
-import styles from './links.module.css';
-import NavLink from './navLink/navLink';
+"use client";
+import Image from "next/image";
+import styles from "./links.module.css";
+import NavLink from "./navLink/navLink";
+import { useState } from "react";
 
 const Links = () => {
   const links = [
     {
-      title: 'Homepage',
-      path: '/',
+      title: "Homepage",
+      path: "/",
     },
     {
-      title: 'About',
-      path: '/about',
+      title: "About",
+      path: "/about",
     },
     {
-      title: 'Contact',
-      path: '/contact',
+      title: "Contact",
+      path: "/contact",
     },
     {
-      title: 'Blog',
-      path: '/blog',
+      title: "Blog",
+      path: "/blog",
     },
   ];
+
+  const [open, setOpen] = useState(false);
+  const session = true;
+  const isAdmin = true;
+  
   return (
     <div className={styles.container}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.path} />
-      ))}
+      <div className={styles.link}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.path} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <Image
+        className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
