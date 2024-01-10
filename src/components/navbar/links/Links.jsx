9b4b68b5dda2a9b523477c76acfa0fb41a572a94
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-10 21:38:43 +0545

    fixed menu error

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,52 +1,52 @@
-"use client";
-import Image from "next/image";
-import styles from "./links.module.css";
-import NavLink from "./navLink/navLink";
-import { useState } from "react";
+'use client';
+import Image from 'next/image';
+import styles from './links.module.css';
+import NavLink from './navLink/navLink';
+import { useState } from 'react';
 
 const Links = () => {
   const links = [
     {
-      title: "Homepage",
-      path: "/",
+      title: 'Homepage',
+      path: '/',
     },
     {
-      title: "About",
-      path: "/about",
+      title: 'About',
+      path: '/about',
     },
     {
-      title: "Contact",
-      path: "/contact",
+      title: 'Contact',
+      path: '/contact',
     },
     {
-      title: "Blog",
-      path: "/blog",
+      title: 'Blog',
+      path: '/blog',
     },
   ];
 
   const [open, setOpen] = useState(false);
   const session = true;
   const isAdmin = true;
-  
+
   return (
     <div className={styles.container}>
-      <div className={styles.link}>
+      <div className={styles.links}>
         {links.map((link) => (
           <NavLink item={link} key={link.path} />
         ))}
         {session ? (
           <>
-            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
             <button className={styles.logout}>Logout</button>
           </>
         ) : (
-          <NavLink item={{ title: "Login", path: "/login" }} />
+          <NavLink item={{ title: 'Login', path: '/login' }} />
         )}
       </div>
       <Image
         className={styles.menuButton}
-        src="/menu.png"
-        alt=""
+        src='/menu.png'
+        alt=''
         width={30}
         height={30}
         onClick={() => setOpen((prev) => !prev)}
