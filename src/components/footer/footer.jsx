e78fb7d34199781f045e7af073ac44cac5a90d8f
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-13 20:59:21 +0545

    created footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,4 +1,14 @@
+import styles from './footer.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Spark's</div>
+      <div className={styles.text}>
+        Spark creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
 };
+
 export default Footer;
