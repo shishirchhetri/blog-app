0c2c54d546044e4fdc11f1330075c04cdb8d38a9
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-20 23:58:43 +0545

    used functionality to render in next

--- src/app/navigationTest/page.jsx
+++ src/app/navigationTest/page.jsx
@@ -1,11 +1,19 @@
-const { default: Link } = require('next/link');
+'use client';
 
+import { useRouter } from 'next/navigation';
+const { default: Link } = require('next/link');
 const NavigationTestPage = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log('CLicked');
+    router.push('/');
+  };
   return (
     <div>
       <Link href='/' prefetch={false}>
         Click here
       </Link>
+      <button onClick={handleClick}>Write and redirect</button>
     </div>
   );
 };
