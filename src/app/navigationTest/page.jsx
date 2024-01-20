37916fa72e573ce23cef22ff14e6cebba8d77096
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-21 00:08:18 +0545

    next navigations

--- src/app/navigationTest/page.jsx
+++ src/app/navigationTest/page.jsx
@@ -1,11 +1,21 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 const { default: Link } = require('next/link');
 const NavigationTestPage = () => {
   const router = useRouter();
+  //getting thr current path from the query tab
+  const pathname = usePathname();
+  //to get the query that is searched like price, name etc
+  const searchParams = useSearchParams();
+  //this q refers the query
+  const q = searchParams.get('q');
+  console.log(q);
+
   const handleClick = () => {
     console.log('CLicked');
+    //if used replace then the histroy would not be stored it back button on the browser won't work
+    //router.replace('/');
     router.push('/');
   };
   return (
