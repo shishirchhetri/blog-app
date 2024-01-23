77889fea4fb9ed001561b13fd0565a96eb9db1d8
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-23 23:16:57 +0545

    added connection to db and created fetch function

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,4 +1,9 @@
 // TEMPORARY DATA
+
+import { Post, User } from './models';
+import { connectToDb } from './utils';
+
+/* 
 const users = [
   { id: 1, name: 'John' },
   { id: 2, name: 'Jane' },
@@ -10,16 +15,57 @@ const posts = [
   { id: 3, title: 'Post 3', body: '......', userId: 2 },
   { id: 4, title: 'Post 4', body: '......', userId: 2 },
 ];
-
+ */
 export const getPosts = async () => {
-  return posts;
+  // return posts;
+  try {
+    //check and connect to mongoose
+    connectToDb();
+    const posts = await Post.find();
+    return posts;
+  } catch (err) {
+    console.log(err);
+    throw new Error('Failed to fetch posts');
+  }
 };
 
-export const getPost = async (id) => {
-  const post = posts.find((post) => post.id === parseInt(id));
-  return post;
+export const getPost = async (slug) => {
+  // const post = posts.find((post) => post.id === parseInt(id));
+  // return post;
+  try {
+    //check and connect to mongoose
+    connectToDb();
+    const post = await Post.find({ slug });
+    return post;
+  } catch (err) {
+    console.log(err);
+    throw new Error('Failed to fetch post');
+  }
+};
+
+//list all users
+export const getUsers = async () => {
+  // return users.find((user) => user.id === id);
+  try {
+    //check and connect to mongoose
+    connectToDb();
+    const users = await User.find();
+    return users;
+  } catch (err) {
+    console.log(err);
+    throw new Error('Failed to fetch users');
+  }
 };
 
 export const getUser = async (id) => {
-  return users.find((user) => user.id === id);
+  // return users.find((user) => user.id === id);
+  try {
+    //check and connect to mongoose
+    connectToDb();
+    const user = await User.findById(id);
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw new Error('Failed to fetch user');
+  }
 };
