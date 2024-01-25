69fdd01f97ffc7e1685e610bcb15b11b72ef40de
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-25 23:58:37 +0545

    added new manual post to the database using server action

--- src/app/serveractiontest/page.jsx
+++ src/app/serveractiontest/page.jsx
@@ -1,10 +1,16 @@
-const { sayHello } = require('@/lib/action');
+const { addPost } = require('@/lib/action');
 
 const ServerActionComponent = () => {
   return (
-    <form action={sayHello}>
-      <button>Say Hello</button>
-    </form>
+    <div>
+      <form action={addPost}>
+        <input type='text' placeholder='Title' name='title' />
+        <input type='desc' placeholder='Description' name='desc' />
+        <input type='text' placeholder='Slug' name='slug' />
+        <input type='text' placeholder='userId' name='userId' />
+        <button>Create Post</button>
+      </form>
+    </div>
   );
 };
 export default ServerActionComponent;
--- src/lib/action.js
+++ src/lib/action.js
@@ -1,4 +1,21 @@
-export const sayHello = async () => {
+import { Post } from './models';
+import { connectToDb } from './utils';
+
+export const addPost = async (formData) => {
   'use server';
-  console.log('Hello');
+  const { title, desc, slug, userId } = Object.fromEntries(formData);
+  try {
+    connectToDb();
+    const newPost = new Post({
+      title,
+      desc,
+      slug,
+      userId,
+    });
+    await newPost.save();
+    console.log('New post saved to Database');
+  } catch (err) {
+    console.log(err);
+    return { error: 'something went wrong while adding post' };
+  }
 };
