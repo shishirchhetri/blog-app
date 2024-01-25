5ebb38d77f129a65f0935d0c52792eaa15474b01
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-26 00:38:31 +0545

    updated delete function

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,8 +1,9 @@
+'use server';
+import { revalidatePath } from 'next/cache';
 import { Post } from './models';
 import { connectToDb } from './utils';
 
 export const addPost = async (formData) => {
-  'use server';
   const { title, desc, slug, userId } = Object.fromEntries(formData);
   try {
     connectToDb();
@@ -14,8 +15,22 @@ export const addPost = async (formData) => {
     });
     await newPost.save();
     console.log('New post saved to Database');
+    revalidatePath('/blog');
   } catch (err) {
     console.log(err);
     return { error: 'something went wrong while adding post' };
+  }
+};
+
+export const deletePost = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+  try {
+    connectToDb();
+    await Post.findByIdAndDelete(id);
+    console.log('Post removed from the database');
+    revalidatePath('/blog');
+  } catch (err) {
+    console.log(err);
+    return { error: 'unknown error occured while deleting the post' };
   }
 };
