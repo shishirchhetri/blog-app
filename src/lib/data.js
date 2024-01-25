f0e5903acb6f96838a114aed3646b36f9fcdb352
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-26 00:38:47 +0545

    update on userdata

--- src/lib/data.js
+++ src/lib/data.js
@@ -68,6 +68,6 @@ export const getUser = async (id) => {
     return user;
   } catch (err) {
     console.log(err);
-    throw new Error('Failed to fetch user');
+    // throw new Error('Failed to fetch user');
   }
 };
