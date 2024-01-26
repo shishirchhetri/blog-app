3f0a38ae04e5c8af83c2cd63775278fb78359570
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-26 12:36:05 +0545

    updates the usage of the app api route for post

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,24 +3,28 @@ import styles from './blog.module.css';
 import { getPosts } from '@/lib/data';
 
 //fetch data with an api
-/* 
+
 const getData = async () => {
   //revalidate will refresh page in the given time
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  // const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  //   next: { revalidate: 3600 },
+  // });
+  // fetching the data with the api route
+  const res = await fetch('http://localhost:3000/api/blog', {
     next: { revalidate: 3600 },
   });
   if (!res.ok) {
-    console.log('something went wrong while fetching data');
+    throw new Error('something went wrong while fetching data');
   }
   return res.json();
 };
- */
+
 const BlogPage = async () => {
   //Fetching data with an api
-  // const posts = await getData();
+  const posts = await getData();
 
   //Fetching posts without an api
-  const posts = await getPosts();
+  // const posts = await getPosts();
   return (
     <div className={styles.container}>
       {posts.map((post) => (
