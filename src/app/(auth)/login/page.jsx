94af32824d54397f492da0c9f9280949557c572d
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-01-31 23:57:12 +0545

    created credentials login for user

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,20 +1,17 @@
-import { handleGithubLogin, login } from '@/lib/action';
-import { auth } from '@/lib/auth';
+import { handleGithubLogin } from '@/lib/action';
+import styles from './login.module.css';
+import LoginForm from '@/components/loginform/LoginForm';
 
-const LoginPage = async () => {
-  const session = await auth();
-  console.log(session);
-
+const LoginPage = () => {
   return (
-    <div>
-      <form action={handleGithubLogin}>
-        <button>Login with Github</button>
-      </form>
-      <form action={login}>
-        <input type='text' placeholder='Enter username' name='username' />
-        <input type='password' placeholder='Enter password' name='password' />
-        <button>Login</button>
-      </form>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <LoginForm />
+        <h5>OR</h5>
+        <form action={handleGithubLogin}>
+          <button className={styles.github}>Login with Github</button>
+        </form>
+      </div>
     </div>
   );
 };
