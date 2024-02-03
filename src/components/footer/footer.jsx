21f0a1a57a0cf02b349e05e249475243adc54005
Shishir Chhetri <shishirchhetri45@gmail.com>
2024-02-04 00:24:00 +0545

    fixed error for deploy at netlify

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import styles from './footer.module.css';
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Spark's</div>
+      <div className={styles.logo}>{`Spark's`}</div>
       <div className={styles.text}>
         Spark creative thoughts agency © All rights reserved.
       </div>
